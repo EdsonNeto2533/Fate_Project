f844afc65777eb49533a9666bedcb98c604e55ec
Edson <edsonneto2533@gmail.com>
2023-07-22 19:05:43 -0300

    feat: adjust modules builds versions

--- buildSrc/src/main/kotlin/dependencies/ConfigurationData.kt
+++ buildSrc/src/main/kotlin/dependencies/ConfigurationData.kt
@@ -2,10 +2,11 @@ package dependencies
 
 object ConfigurationData {
     const val applicationId = "com.mctable.fateproject"
-    const val compileSdk = 32
+    const val compileSdk = 33
     const val buildToolsVersion = "30.0.3"
     const val minSdk = 24
     const val targetSdk= 33
+    const val jvmTarget = "17"
     const val versionCode = 1
     const val versionName = "1.0.0"
 }
