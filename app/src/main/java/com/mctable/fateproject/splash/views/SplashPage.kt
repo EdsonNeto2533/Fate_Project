cbf7e0a0a5f3e3a99a3c3c18896602d655bb1d26
Edson <edsonneto2533@gmail.com>
2023-08-13 19:03:10 -0300

    feat: create SplashPage initial layout

--- app/src/main/java/com/mctable/fateproject/splash/views/SplashPage.kt
+++ app/src/main/java/com/mctable/fateproject/splash/views/SplashPage.kt
@@ -1,31 +1,40 @@
 package com.mctable.fateproject.splash.views
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun SplashPage() {
     Scaffold(
+        modifier = Modifier.fillMaxSize(),
         content = { innerPadding ->
             Column(
-                modifier = Modifier.consumeWindowInsets(innerPadding)
+                modifier = Modifier
+                    .consumeWindowInsets(innerPadding)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
                     text = "Welcome",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .align(Alignment.CenterHorizontally)
+                    modifier = Modifier.padding(16.dp)
                 )
+                CircularProgressIndicator()
             }
         },
     )
