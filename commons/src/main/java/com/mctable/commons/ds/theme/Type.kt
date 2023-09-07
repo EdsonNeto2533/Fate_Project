19bd9bc3b84e0d56e58db1cc8fae2aac66a206b3
Edson <edsonneto2533@gmail.com>
2023-09-07 16:25:35 -0300

    feat: Adjust fonts

--- commons/src/main/java/com/mctable/commons/ds/theme/Type.kt
+++ commons/src/main/java/com/mctable/commons/ds/theme/Type.kt
@@ -1,34 +1,86 @@
 package com.mctable.commons.ds.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.mctable.commons.R
+
+/**
+ * Example of use
+ * Text(
+ *     text = "Customized TextStyle (Body1) ",
+ *     style = MaterialTheme.typography.bodySmall
+ * )
+ */
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+    headlineSmall = TextStyle(
+        fontSize = 20.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontWeight = FontWeight(500),
+        color = Color(0xFF1D1D1D),
+        textAlign = TextAlign.Center,
     ),
+    headlineMedium = TextStyle(
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        fontWeight = FontWeight(500),
+        color = Color(0xFF1D1D1D),
+        textAlign = TextAlign.Center,
+    ),
+    headlineLarge = TextStyle(
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        fontWeight = FontWeight(500),
+        color = Color(0xFF1D1D1D),
+        textAlign = TextAlign.Center,
+    ),
+    //token - subtitle 3
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 10.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontWeight = FontWeight(700),
+        color = Color(0xFFFFFFFF),
+    ),
+    //token - subtitle 2
+    labelMedium = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight(700),
+        color = Color(0xFFFFFFFF),
+    ),
+    //token - subtitle 1
+    labelLarge = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight(700),
+        color = Color(0xFFFFFFFF),
+    ),
+    //token - body 3
+    bodySmall = TextStyle(
+        fontSize = 10.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight(400),
+        color = Color(0xFFFFFFFF),
+    ),
+    //token - body 2
+    bodyMedium = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight(400),
+        color = Color(0xFFFFFFFF),
+    ),
+    //token body 1
+    bodyLarge = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight(400),
+        color = Color(0xFFFFFFFF),
+    ),
 )
