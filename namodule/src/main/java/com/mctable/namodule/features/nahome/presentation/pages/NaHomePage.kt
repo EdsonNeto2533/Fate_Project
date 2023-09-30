348ae9c31fb27151272de4b21fe3b9f514f92cf4
Edson <edsonneto2533@gmail.com>
2023-09-30 17:37:36 -0300

    feat: create shimmerBrush

--- namodule/src/main/java/com/mctable/namodule/features/nahome/presentation/pages/NaHomePage.kt
+++ namodule/src/main/java/com/mctable/namodule/features/nahome/presentation/pages/NaHomePage.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
@@ -22,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.mctable.commons.ds.components.shimmerBrush
 import com.mctable.core.utils.classes.UIState
 import com.mctable.core.utils.extensions.getViewModel
 import com.mctable.namodule.NaMainActivity
@@ -48,7 +50,12 @@ fun NaHomePage() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        CircularProgressIndicator()
+                        Box(
+                            modifier = Modifier
+                                .background(shimmerBrush(targetValue = 1300f))
+                                .fillMaxWidth()
+                                .height(200.dp)
+                        )
                     }
 
                 }
@@ -83,10 +90,10 @@ fun NaHomePage() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text(
-                            text = state.data[0].name,
-                            modifier = Modifier
-                        )
+//                        Text(
+//                            text = state.data[0].name,
+//                            modifier = Modifier
+//                        )
                     }
                 }
             }
