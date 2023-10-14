8c9730e9c89f870ad2843798e22d8072f1632a8c
Edson <edsonneto2533@gmail.com>
2023-10-14 20:34:17 -0300

    feat: adjust defaultAppBarComponent to attend to material 3 pattern

--- commons/src/main/java/com/mctable/commons/ds/components/DefaultAppBarComponent.kt
+++ commons/src/main/java/com/mctable/commons/ds/components/DefaultAppBarComponent.kt
@@ -1,39 +1,37 @@
 package com.mctable.commons.ds.components
 
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.invisibleToUser
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.mctable.commons.R
 import com.mctable.commons.ds.theme.md_theme_light_onPrimary
 import com.mctable.commons.ds.theme.md_theme_light_surfaceTint
+import com.mctable.commons.ds.utils.emptyString
+
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun defaultAppBarComponent(
     modifier: Modifier = Modifier,
-    title: String?,
+    title: String? = null,
     backButtonClick: (() -> Unit)? = null
 ) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         modifier = modifier.then(modifier),
         colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = md_theme_light_surfaceTint),
         navigationIcon = {
@@ -48,33 +46,22 @@ fun defaultAppBarComponent(
                         tint = md_theme_light_onPrimary
                     )
                 }
-            } ?: run {
-                Icon(
-                    modifier = Modifier
-                        .width(50.dp)
-                        .semantics {
-                            this.invisibleToUser()
-                        },
-                    painter = painterResource(id = R.drawable.fgo_logo),
-                    contentDescription = "",
-                    tint = md_theme_light_onPrimary
-                )
             }
-
         },
         title = {
             title?.let {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 16.dp)
-                ) {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.headlineMedium,
-                        color = md_theme_light_onPrimary
-                    )
-                }
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = md_theme_light_onPrimary
+                )
+            } ?: run {
+                Icon(
+                    modifier = Modifier.height(28.dp),
+                    painter = painterResource(id = R.drawable.fgo_logo),
+                    contentDescription = emptyString,
+                    tint = md_theme_light_onPrimary
+                )
             }
         }
     )
