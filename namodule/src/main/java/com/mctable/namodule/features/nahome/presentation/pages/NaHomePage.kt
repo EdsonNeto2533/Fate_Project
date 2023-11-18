9b0636867002bf1845a50883a8fc9d47c368da58
Edson <edsonneto2533@gmail.com>
2023-11-18 15:23:07 -0300

    feat: remove unused imports from NaHomePage

--- namodule/src/main/java/com/mctable/namodule/features/nahome/presentation/pages/NaHomePage.kt
+++ namodule/src/main/java/com/mctable/namodule/features/nahome/presentation/pages/NaHomePage.kt
@@ -1,9 +1,7 @@
 package com.mctable.namodule.features.nahome.presentation.pages
 
 
-import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
