23aac4d1b507fa095a01657ee9fc7fca84de9906
Edson <edsonneto2533@gmail.com>
2024-09-29 18:01:50 -0300

    feat: Adjust DI for SplashViewModel and navigation

--- app/src/main/java/com/mctable/fateproject/commons/di/MainProvider.kt
+++ app/src/main/java/com/mctable/fateproject/commons/di/MainProvider.kt
@@ -7,11 +7,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object MainProvider {
 
+    @Singleton
     @Provides
     fun appNavigation(): FateNavigation =
         FateNavigationImpl(startDestination = Routes.SPLASH)
--- app/src/main/java/com/mctable/fateproject/splash/presentation/viewmodel/SplashPageViewModel.kt
+++ app/src/main/java/com/mctable/fateproject/splash/presentation/viewmodel/SplashPageViewModel.kt
@@ -3,8 +3,10 @@ package com.mctable.fateproject.splash.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import com.mctable.commons.ds.navigation.FateNavigation
 import com.mctable.commons.ds.navigation.Routes
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class SplashPageViewModel @Inject constructor(
     private val fateNavigation: FateNavigation
 ) : ViewModel() {
