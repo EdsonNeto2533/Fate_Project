4f95224a18b51fbe220d5ab63e4452d8e7453f27
Edson <edsonneto2533@gmail.com>
2025-05-02 17:08:16 -0300

    feat: add navigation animation to NaHomePage

--- namodule/src/main/java/com/mctable/namodule/navigation/NaModuleNavigation.kt
+++ namodule/src/main/java/com/mctable/namodule/navigation/NaModuleNavigation.kt
@@ -1,12 +1,19 @@
 package com.mctable.namodule.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.mctable.commons.ds.navigation.Routes
 import com.mctable.namodule.features.nahome.presentation.pages.NaHomePage
 
 fun NavGraphBuilder.naModuleNavigation() {
-    composable(Routes.NA_HOME_PAGE) {
+    composable(Routes.NA_HOME_PAGE, enterTransition = {
+        slideIntoContainer(
+            AnimatedContentTransitionScope.SlideDirection.Left,
+            animationSpec = tween(700)
+        )
+    }) {
         NaHomePage()
     }
 }
